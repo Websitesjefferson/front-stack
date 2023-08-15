0a98339b92d04acf73ba6a959b20b7e33c0109c8
Jefferson <jeffersonneto1991@gmail.com>
2023-08-14 21:36:22 -0300

    initial commit

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.scss';
 import CloseIcon from '@mui/icons-material/Close';
 import { Link } from 'react-router-dom'
 
-import Logo from '../../assets/logon.png'
+import Logo from '../../assets/Logon.png'
 export default function Header() {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
@@ -19,7 +19,7 @@ export default function Header() {
       <AppBar >
         <Toolbar> 
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <img className={styles.img} src={Logo} alt="" />
+              <img className={styles.img} src={Logo} alt="Logo" />
           </Typography>
           <Hidden mdUp implementation="css">
             {/* O ícone do menu só será exibido em telas menores ou iguais a 900 pixels */}
